08b53ae0bcdd9a16ce70379ee52d122d30fc7eca
milonsarker <milon.sarker@gmail.com>
2022-05-21 20:04:15 +0600

    Continuous Commit

--- LeetCode/DataStructure/C_Plus_Plus/88.cpp
+++ LeetCode/DataStructure/C_Plus_Plus/88.cpp
@@ -28,3 +28,31 @@ class Solution
             }
         }
 };
+
+//Three Pointers (Start From the End)
+class Solution
+{
+    public:
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+        {
+            int p1 = m - 1;
+            int p2 = n - 1;
+            for ( int i = m + n - 1; i >= 0; i--)
+            {
+                if (p2 < 0)
+                {
+                    break;
+                }
+                if (p1 >= 0 and nums1[p1] > nums2[p2])
+                {
+                    nums1[i] = nums1[p1];
+                    p1--;
+                }
+                else
+                {
+                    nums1[i] = nums2[p2];
+                    p2--;
+                }
+            }
+        }
+};
