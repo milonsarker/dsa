b410607179dcded0a93c2959ee6a288ffab55581
milonsarker <milon.sarker@gmail.com>
2022-06-03 09:58:35 +0600

    Continuous Commit

--- LeetCode/Algorithm/Python/189.py
+++ LeetCode/Algorithm/Python/189.py
@@ -20,4 +20,14 @@ class Solution:
                     nums[x] = pre
                     pre = temp
 
-'''S'''
+'''Using Extra Array'''
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        a = [0] * n
+        for i in range(n):
+            a[(i + k) % n] = nums[i]
+        nums[:] = a
