acb87d643995fd5b6be9a2331c8a7bb655cc6fe1
Milon Sarker <milon.sarker@gmail.com>
2022-10-09 21:26:01 -0500

    Regular Development

--- LeetCode/Study_Plan/DataStructure/Python/36.py
+++ LeetCode/Study_Plan/DataStructure/Python/36.py
@@ -2,30 +2,49 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def row_check(board):
+        def row_col_check(board):
             for i in range(9):
-                cnt_dict = {}
-                for j in board[i][:]:
-                    if j != ".":
-                        if j in cnt_dict:
+                row_cnt_dict = {}
+                col_cnt_dict = {}
+                for j in range(9):
+                    if board[i][j] != ".":
+                        if board[i][j] in row_cnt_dict:
                             return False
                         else:
-                            cnt_dict[j] = 1
+                            row_cnt_dict[board[i][j]] = 1
+                    if board[j][i] != ".":
+                        if board[j][i] in col_cnt_dict:
+                            return False
+                        else:
+                            col_cnt_dict[board[j][i]] = 1
             return True
-        row_status = row_check(board)
+        row_status = row_col_check(board)
+        print(row_status)
 
-        def col_check(board):
-            print('ll')
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                block_cnt_dict = {}
+                for k in range(3):
+                    for l in range(3):
+                        if board[i + k][j + l] != ".":
+                            if board[i + k][j + l] in block_cnt_dict:
+                                return False
+                            else:
+                                block_cnt_dict[board[i + k][j + l]] = 1
+                        #print(str(i + k) + ',' + str(j + l), end = " ")
+                    #print()
+                #print('--------------')
+        return row_status
+
 if __name__=="__main__":
-    board =  [["5", "3", ".", ".", "7", ".", ".", ".", "."]
-            , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
-            , [".", "9", "8", ".", ".", ".", ".", "6", "."]
-            , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
-            , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
-            , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
-            , [".", "6", ".", ".", ".", ".", "2", "8", "."]
-            , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
-            , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
-    #solObj = Solution()
-    #solObj.isValidSudoku(board)
-    print(board[1][:])
+    board = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
+             ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+             [".", "9", "8", ".", ".", ".", ".", "6", "."],
+             ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+             ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+             ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+             [".", "6", ".", ".", ".", ".", "2", "8", "."],
+             [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+             [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+    solObj = Solution()
+    solObj.isValidSudoku(board)
