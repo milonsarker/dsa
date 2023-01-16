969807704090845c3b518fe70e66a46f1fb9cef5
Milon Sarker <milon.sarker@gmail.com>
2023-01-16 01:39:42 -0600

    Regular Development

--- LeetCode/Daily_Problem/Python/57.py
+++ LeetCode/Daily_Problem/Python/57.py
@@ -20,3 +20,23 @@ class Solution:
         result.append([x, y])
         return result
 
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        x, y = newInterval[0], newInterval[1]
+        for i in range(len(intervals)):
+            m = intervals[i][0]
+            n = intervals[i][1]
+            if n < x:
+                result.append(intervals[i])
+            elif m > y:
+                result.append([x,y])
+                result.extend(intervals[i:])
+                return result
+            elif n >= x or m <= y:
+                x = min(m, x)
+                y = max(n, y)
+        result.append([x,y])
+        return result
