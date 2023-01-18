a7b5019c11f1cf91947312a23af2a40fbe71f877
Milon Sarker <milon.sarker@gmail.com>
2023-01-17 20:48:45 -0600

    Regular Development

---
+++ LeetCode/Daily_Problem/Python/918.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/maximum-sum-circular-subarray/description/
+
+
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        arrSum = nums[0]
+        aiSum, aiMaxSum = nums[0], nums[0]
+        nSum, nMaxSum = -nums[0], -nums[0]
+        for i in nums[1:]:
+            arrSum += i
+
+            #Positive part
+            aiSum = max(i, aiSum + i)
+            aiMaxSum = max(aiMaxSum, aiSum)
+
+            #Negative part
+            temp_val = - i
+            nSum = max(temp_val, nSum + temp_val)
+            nMaxSum = max(nMaxSum, nSum)
+
+        cirSum = arrSum + nMaxSum
+
+        print(arrSum, cirSum, aiMaxSum, nMaxSum)
+        if cirSum > aiMaxSum and cirSum != 0:
+            return cirSum
+        else:
+            return aiMaxSum
