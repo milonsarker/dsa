20129bb2558f490f75392f28e6e38ba0a4598721
Milon Sarker <milon.sarker@gmail.com>
2023-02-04 17:28:39 -0600

    Regular Development

---
+++ LeetCode/Daily_Problem/Python/567.py
@@ -0,0 +1,23 @@
+#https://leetcode.com/problems/permutation-in-string/
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        sdic = {}
+        for i in s1:
+            if i in sdic:
+                sdic[i] += 1
+            else:
+                sdic[i] = 1
+        cpy = dict(sdic)
+        for j in range(len(s2) - len(s1) + 1):
+            cpy = dict(sdic)
+            for i in s2[j: j + len(s1)]:
+                    if i in cpy:
+                        if cpy[i] == 1:
+                            cpy.pop(i)
+                        else:
+                            cpy[i] -= 1
+            if bool(cpy) == False:
+                return True
+        return False
