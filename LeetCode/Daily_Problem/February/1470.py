a2e43066704e1e5afc52ae796a85366cd0eb2020
Milon Sarker <milon.sarker@gmail.com>
2023-02-08 03:28:08 -0600

    Regular Development

---
+++ LeetCode/Daily_Problem/February/1470.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/shuffle-the-array/description/
+
+#Learn
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = [0] * (2 * n)
+        for i in range(n):
+            result[2 * i] = nums[i]
+            result[2 * i + 1] = nums[n + i]
+        return result
