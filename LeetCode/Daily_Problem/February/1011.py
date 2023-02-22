efe705407b9ea186c5beb5f9140e314f0d32d9a8
mms_macmini <milon.sarker@gmail.com>
2023-02-22 16:37:00 -0600

    Regular Problem Solving

---
+++ LeetCode/Daily_Problem/February/1011.py
@@ -0,0 +1,28 @@
+#https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/description/
+
+#Learn
+
+class Solution:
+    def shipWithinDays(self, weights: List[int], D: int) -> int:
+        # Set the range of possible weight capacities
+        left = max(weights)
+        right = sum(weights)
+
+        while left < right:
+            mid = (left + right) // 2
+            day_count = 1
+            total_weight = 0
+
+            for weight in weights:
+                total_weight += weight
+
+                if total_weight > mid:
+                    day_count += 1
+                    total_weight = weight
+
+            if day_count > D:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
