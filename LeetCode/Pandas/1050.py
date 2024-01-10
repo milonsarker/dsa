a58960f6b5d3cf7abbb03d3964d9172a46cfc91e
Milon Sarker <milon.sarker@gmail.com>
2024-01-10 16:43:55 +0000

    regular problem solving

---
+++ LeetCode/Pandas/1050.py
@@ -0,0 +1,8 @@
+#https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director.groupby(['actor_id', 'director_id']).size().reset_index(name='count')
+    df = df[df['count']>=3]
+    return df[['actor_id', 'director_id']]
