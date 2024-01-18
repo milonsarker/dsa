668e9ee9a2439b4baeb46f74ae4350b4f90bfe0b
Milon Sarker <milon.sarker@gmail.com>
2024-01-18 05:31:18 +0000

    rregular change

---
+++ LeetCode/Array/Python/2942.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        result = []
+        for i, word in enumerate(words):
+            if x in word: 
+                result.append(i)
+        return result
