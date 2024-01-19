ae1ffc6d313f53e6d120db0ae5a257ceb85e8959
Milon Sarker <milon.sarker@gmail.com>
2024-01-19 20:54:19 +0000

    regular problem solving

---
+++ LeetCode/Pandas/2891.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/method-chaining/
+
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals['weight']>100].sort_values(by = 'weight', ascending = False)[['name']]
+    
