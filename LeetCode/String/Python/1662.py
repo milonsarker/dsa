a485fd85c5c933563dc828a986d52bc657c59d17
Milon Sarker <milon.sarker@gmail.com>
2024-01-30 03:04:36 +0000

    regular problem solving

--- LeetCode/String/Python/1662.py
+++ LeetCode/String/Python/1662.py
@@ -2,3 +2,19 @@
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         return ''.join(word1) == ''.join(word2)
+
+
+#https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        for a, b in zip(self.generate(word1), self.generate(word2)):
+            if a != b:
+                return False
+        return True
+        
+
+    def generate(self, wordlist: List[str]):
+        for word in wordlist:
+            for ch in word:
+                yield ch
+        yield None
