64e64c460a1a23f2fe64e28067863011e87de8f5
Milon Sarker <milon.sarker@gmail.com>
2024-09-23 14:24:06 +0000

    ps

---
+++ LeetCode/Databases/MySQL/3059.sql
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/find-all-unique-email-domains/
+# Write your MySQL query statement below
+
+select substring(email, instr(email, '@') + 1) email_domain, count(*) count   
+    from emails
+    where email like '%com'
+    group by substring(email, instr(email, '@') + 1)
+    order by count(*) desc
+    ;
