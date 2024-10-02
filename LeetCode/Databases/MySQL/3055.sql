5813a99bb00a38112472301e4f4f5dc6c890a3f3
Milon Sarker <milon.sarker@gmail.com>
2024-10-02 14:14:47 +0000

    ps

--- LeetCode/Databases/MySQL/3055.sql
+++ LeetCode/Databases/MySQL/3055.sql
@@ -3,10 +3,10 @@
 
 with cte_rnk as 
 (
-    select policy_id, state, fraud_score, rank() over (partition by state order by fraud_score desc) as rnk
+    select policy_id, state, fraud_score, percent_rank() over (partition by state order by fraud_score desc) as rnk
         from fraud
 )
 select policy_id, state, fraud_score
     from cte_rnk
-    where rnk = 1
+    where rnk < 0.05
     order by state, fraud_score desc, policy_id
